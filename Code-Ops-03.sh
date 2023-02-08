b07593b83bb8b7cbdcf1000cbdc5909ad292f994
Sierra <maldonado.sierra.a@gmail.com>
2023-02-08 23:22:15 +0000

    Changes

--- Code-Ops-03.sh
+++ Code-Ops-03.sh
@@ -1,4 +1,4 @@
-# !/bin/bash
+#!/bin/bash
 # Code file 03
 # Sierra Maldonado
 # Fuctions
@@ -6,8 +6,13 @@
 
 #main
 
--user
-
+print_fuctions () { 
+ cat /var/log/auth.log   
+    }
+print_fuctions
+var_change () {
+    local var1=local 1
+}
 
 
 
