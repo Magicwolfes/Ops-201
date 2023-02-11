7ccb3796d01f031d471f4396fdeca381e4f0a181
Sierra <maldonado.sierra.a@gmail.com>
2023-02-10 19:03:32 -0500

    updates

---
+++ Code-Ops-05.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Loops
+# Sierra Maldonado
+# 10Feb23
+
+while true; do
+    # Display running processes
+    echo "ps -ef"
+    ps -ef
+    # Ask user for PID
+    read -p "Enter the PID of the process you want to kill: " pid
+    # Kill the process
+    kill -9 $pid
+    # Confirm that the process was killed
+done
